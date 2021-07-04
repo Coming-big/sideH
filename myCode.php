1d483402b89725a37406caba91543aa5318f0af8
Coming-big <86569180+Coming-big@users.noreply.github.com>
2021-07-04 21:11:06 +0100

    Add files via upload

--- myCode.php
+++ myCode.php
@@ -1,29 +1,5 @@
 <?php 
 
-$string = "Hello World";
-
-echo $string  .  "<br>";
-
-$number = 345678;
-
-echo $number .  "<br>";
-
-$float = 34.5;
-
-echo $float .  "<br>";
-
-$boolean = true;
-
-echo $boolean .  "<br>";
-
-$name = array("Amos", "paul", "Sola", "kay");
-
-var_dump($name);
-
-echo  "<br>";
-
-echo $name[0];
-
-
+echo "Hello World";
 
 ?>
